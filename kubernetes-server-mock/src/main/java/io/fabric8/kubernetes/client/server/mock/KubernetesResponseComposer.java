b63b0a95b8e38c3011b295eb38b9301bc63f060a
Steve Wolter <swolter@google.com>
2018-05-29 17:28:17 +0200

    Fix list building for custom resources.
    This fixes issue #1087. Custom resources cannot be deserialized with the
    standard Kubernetes JSON parser because it doesn't know about them.
    Since all items are JSON anyway, we can just use text concatenation
    anyway.

--- kubernetes-server-mock/src/main/java/io/fabric8/kubernetes/client/server/mock/KubernetesResponseComposer.java
+++ kubernetes-server-mock/src/main/java/io/fabric8/kubernetes/client/server/mock/KubernetesResponseComposer.java
@@ -15,34 +15,16 @@
  */
 package io.fabric8.kubernetes.client.server.mock;
 
-import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.api.model.KubernetesList;
-import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
-import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.utils.Serialization;
 import io.fabric8.mockwebserver.crud.ResponseComposer;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class KubernetesResponseComposer implements ResponseComposer {
 
   @Override
   public String compose(Collection<String> collection) {
-    List<HasMetadata> items = new ArrayList<>();
-    for (String item : collection) {
-      try (InputStream stream = new ByteArrayInputStream(item.getBytes(StandardCharsets.UTF_8.name()))) {
-        HasMetadata h = Serialization.unmarshal(stream);
-        items.add(h);
-      } catch (Exception e) {
-        throw KubernetesClientException.launderThrowable(e);
-      }
-    }
-    KubernetesList list = new KubernetesListBuilder().withItems(items).build();
-    return Serialization.asJson(list);
+    return String.format(
+        "{\"apiVersion\":\"v1\",\"kind\":\"List\", \"items\": [%s]}",
+        String.join(",", collection));
   }
 }
